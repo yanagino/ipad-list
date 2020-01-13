706d135bca3b1a432d87a8c9d9f3621389eff0fa
maximum-erika <ec2-user@ip-172-31-28-44.ec2.internal>
2020-01-13 12:05:59 +0000

    heroku validate

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   validates :attendance, presence: true, length: { maximum: 4 },
-                    format: { with: /\A[0-9]+[a-z]+[0-99]+\z/i }
+                    format: { with: /\A\d{1}[a-z]\d{1,2}\z/i }
   validates :number, presence: true, length: { maximum: 3 },
                     numericality: { less_than_or_equal_to: 100, greater_than_or_equal_to: 1 }
 end
